d20d8997508232c5d7838be3a9a119293642fb36
Md Asikuzzaman <mdasikuzzaman.en@gmail.com>
2023-03-26 16:42:47 +0600

    new style added

--- pages/signup/[[...index]].tsx
+++ pages/signup/[[...index]].tsx
@@ -37,115 +37,117 @@ const Index: NextPage<Props> = ({}) => {
   };
 
   return (
-    <div className='h-screen bg-gradient-to-tr to-indigo-500 from-teal-500 grid place-content-center'>
-      <form className='w-[400px] bg-white rounded-md overflow-hidden shadow-md py-8 px-6'>
-        <h2 className='text-center text-2xl font-semibold mb-6'>Sign Up</h2>
+    <div className='h-screen bg-gradient-to-tr to-indigo-500 from-teal-500 flex justify-center items-center'>
+      <div className='shrink-0 max-w-[430px] w-full px-3'>
+        <form className='bg-white rounded-md overflow-hidden shadow-md py-8 px-6'>
+          <h2 className='text-center text-2xl font-semibold mb-6 text-fuchsia-800'>Sign Up</h2>
 
-        <label htmlFor='username'>Username *</label>
-        <div className='relative mb-4 mt-1'>
-          <AiOutlineUser className='absolute top-0 left-3 bottom-0 my-auto text-base' />
-          <input
-            type='text'
-            placeholder='Username'
-            name='username'
-            id='username'
-            className='w-full outline-none bg-slate-100 pl-10 py-2 pr-2 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
-          />
-        </div>
-
-        <label htmlFor='email'>Email *</label>
-        <div className='relative mb-4 mt-1'>
-          <MdAlternateEmail className='absolute top-0 left-3 bottom-0 my-auto text-base' />
-          <input
-            type='text'
-            placeholder='Email Address'
-            name='email'
-            id='email'
-            className='w-full outline-none bg-slate-100 pl-10 py-2 pr-2 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
-          />
-        </div>
-
-        <label htmlFor='password'>Password *</label>
-        <div className='relative mb-4 mt-1'>
-          <MdLockOutline className='absolute top-0 left-3 bottom-0 my-auto text-base' />
-          <input
-            type={showPassword ? 'text' : 'password'}
-            placeholder='Password'
-            name='password'
-            id='password'
-            className='w-full outline-none bg-slate-100 pl-10 py-2 pr-10 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
-          />
-          <div className='absolute top-0 right-3 bottom-0 my-auto text-base  grid place-content-center'>
-            {showPassword ? (
-              <AiOutlineEyeInvisible
-                onClick={handleHiddenPassword}
-                className='cursor-pointer'
-              />
-            ) : (
-              <AiOutlineEye
-                onClick={handleShowPassword}
-                className='cursor-pointer'
-              />
-            )}
+          <label htmlFor='username'>Username *</label>
+          <div className='relative mb-4 mt-1'>
+            <AiOutlineUser className='absolute top-0 left-3 bottom-0 my-auto text-base' />
+            <input
+              type='text'
+              placeholder='Username'
+              name='username'
+              id='username'
+              className='w-full outline-none bg-slate-100 pl-10 py-2 pr-2 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
+            />
           </div>
-        </div>
 
-        <label htmlFor='cpassword'>Confirm Password *</label>
-        <div className='relative mt-1'>
-          <MdLockOutline className='absolute top-0 left-3 bottom-0 my-auto text-base' />
-          <input
-            type={showConfirmPassword ? 'text' : 'password'}
-            placeholder='Confirm Password'
-            name='cpassword'
-            id='cpassword'
-            className='w-full outline-none bg-slate-100 pl-10 py-2 pr-10 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
-          />
-          <div className='absolute top-0 right-3 bottom-0 my-auto text-base  grid place-content-center'>
-            {showConfirmPassword ? (
-              <AiOutlineEyeInvisible
-                onClick={handleConfirmHiddenPassword}
-                className='cursor-pointer'
-              />
-            ) : (
-              <AiOutlineEye
-                onClick={handleConfirmShowPassword}
-                className='cursor-pointer'
-              />
-            )}
+          <label htmlFor='email'>Email *</label>
+          <div className='relative mb-4 mt-1'>
+            <MdAlternateEmail className='absolute top-0 left-3 bottom-0 my-auto text-base' />
+            <input
+              type='text'
+              placeholder='Email Address'
+              name='email'
+              id='email'
+              className='w-full outline-none bg-slate-100 pl-10 py-2 pr-2 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
+            />
           </div>
-        </div>
 
-        <button className='uppercase bg-fuchsia-800 w-full mt-5 py-1 text-white rounded-3xl select-none'>
-          Sign Up
-        </button>
-        <p className='text-center py-4'>Or</p>
-        <div className='flex justify-center gap-3'>
-          <span className='h-9 w-9 bg-slate-200 rounded-full grid place-content-center cursor-pointer shadow-md'>
-            <FcGoogle className='text-xl' />
-          </span>
+          <label htmlFor='password'>Password *</label>
+          <div className='relative mb-4 mt-1'>
+            <MdLockOutline className='absolute top-0 left-3 bottom-0 my-auto text-base' />
+            <input
+              type={showPassword ? 'text' : 'password'}
+              placeholder='Password'
+              name='password'
+              id='password'
+              className='w-full outline-none bg-slate-100 pl-10 py-2 pr-10 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
+            />
+            <div className='absolute top-0 right-3 bottom-0 my-auto text-base  grid place-content-center'>
+              {showPassword ? (
+                <AiOutlineEyeInvisible
+                  onClick={handleHiddenPassword}
+                  className='cursor-pointer'
+                />
+              ) : (
+                <AiOutlineEye
+                  onClick={handleShowPassword}
+                  className='cursor-pointer'
+                />
+              )}
+            </div>
+          </div>
 
-          <span className='h-9 w-9 bg-blue-800 rounded-full grid place-content-center cursor-pointer shadow-md'>
-            <GrFacebookOption className='text-xl text-white' />
-          </span>
+          <label htmlFor='cpassword'>Confirm Password *</label>
+          <div className='relative mt-1'>
+            <MdLockOutline className='absolute top-0 left-3 bottom-0 my-auto text-base' />
+            <input
+              type={showConfirmPassword ? 'text' : 'password'}
+              placeholder='Confirm Password'
+              name='cpassword'
+              id='cpassword'
+              className='w-full outline-none bg-slate-100 pl-10 py-2 pr-10 rounded-sm border-slate-100 focus:border-fuchsia-800 border-b-2'
+            />
+            <div className='absolute top-0 right-3 bottom-0 my-auto text-base  grid place-content-center'>
+              {showConfirmPassword ? (
+                <AiOutlineEyeInvisible
+                  onClick={handleConfirmHiddenPassword}
+                  className='cursor-pointer'
+                />
+              ) : (
+                <AiOutlineEye
+                  onClick={handleConfirmShowPassword}
+                  className='cursor-pointer'
+                />
+              )}
+            </div>
+          </div>
 
-          <span className='h-9 w-9 bg-black rounded-full grid place-content-center cursor-pointer shadow-md'>
-            <AiOutlineGithub className='text-xl text-white' />
-          </span>
+          <button className='uppercase bg-fuchsia-800 w-full mt-5 py-1 text-white rounded-3xl select-none'>
+            Sign Up
+          </button>
+          <p className='text-center py-4'>Or</p>
+          <div className='flex justify-center gap-3'>
+            <span className='h-9 w-9 bg-slate-200 rounded-full grid place-content-center cursor-pointer shadow-md'>
+              <FcGoogle className='text-xl' />
+            </span>
 
-          <span className='h-9 w-9 bg-sky-600 rounded-full grid place-content-center cursor-pointer shadow-md'>
-            <AiOutlineTwitter className='text-xl text-white' />
-          </span>
-        </div>
-        <p className='text-center mt-8 text-slate-600 text-sm'>
-          Already have an account?
-          <Link
-            className='hover:underline text-fuchsia-800 ml-1'
-            href='/signin'
-          >
-            Sign in
-          </Link>
-        </p>
-      </form>
+            <span className='h-9 w-9 bg-blue-800 rounded-full grid place-content-center cursor-pointer shadow-md'>
+              <GrFacebookOption className='text-xl text-white' />
+            </span>
+
+            <span className='h-9 w-9 bg-black rounded-full grid place-content-center cursor-pointer shadow-md'>
+              <AiOutlineGithub className='text-xl text-white' />
+            </span>
+
+            <span className='h-9 w-9 bg-sky-600 rounded-full grid place-content-center cursor-pointer shadow-md'>
+              <AiOutlineTwitter className='text-xl text-white' />
+            </span>
+          </div>
+          <p className='text-center mt-8 text-slate-600 text-sm'>
+            Already have an account?
+            <Link
+              className='hover:underline text-fuchsia-800 ml-1'
+              href='/signin'
+            >
+              Sign in
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
